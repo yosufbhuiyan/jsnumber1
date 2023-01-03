9dc04e6ee9c7a96810f321191665198c575e6e93
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-03 21:44:12 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var alo = 10;
+var piyaz = 20;
+
+var motTaka =  alo + piyaz;
+
+console.log(motTaka);
+
+
+var boy1 = 44;
+var boy2 = 45;
+var boy3 = 46;
+var boy4 = 47;
+var boy5 = 48;
+
+var boysResult = boy1 +boy2 + boy3 + boy4 + boy5;
+
+console.log(boysResult)
+
+
+
+var boy1 = 100;
+
+var boy2 = 34;
+
+var diference = boy1 - boy2 ;
+
+console.log('The boys diffent marks is ' +   diference );
